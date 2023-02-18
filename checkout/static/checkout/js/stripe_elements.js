e5198b09ed8362cac95a278af7895fc178b8cfbc
Anu Sethi <anu1301@hotmail.co.uk>
2023-02-18 18:16:37 +0000

    Updated webhook handler to handle profiles

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -120,7 +120,7 @@ form.addEventListener('submit', function (ev) {
             }
         }
     });
-    }).fail(function() {
+}).fail(function() {
         // Reloads the page - the error will be in django messages
         location.reload();
     })
