4200860e0b48b1490060c07bbc7899dc125999a0
Anu Sethi <anu1301@hotmail.co.uk>
2023-02-23 18:58:10 +0000

    Added experiences views and templates

--- wine_tasting/views.py
+++ wine_tasting/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Experiences
 
 # Create your views here.
+
+
+def all_experiences(request):
+    """ A view to show all experiences """
+
+    experiences = Experiences.objects.all()
+
+    context = {
+        'experiences': experiences,
+    }
+
+    return render(request, 'experiences/experiences.html', context)
+
+
+
