193bc89f4d9383683608d5983f43c4640eb382bd
Anu Sethi <anu1301@hotmail.co.uk>
2023-02-25 13:18:41 +0000

    Created wine tasting/experiences page and linked home page to template

--- wine_tasting/models.py
+++ wine_tasting/models.py
@@ -7,6 +7,7 @@ class Experiences(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2)
     content = models.TextField()
     image = models.ImageField()
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
 
     def __str__(self):
         return self.name
