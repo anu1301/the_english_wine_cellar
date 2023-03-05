2958cc03e993bc469dd8b03268cd81c41618e886
Anu Sethi <anu1301@hotmail.co.uk>
2023-03-05 23:52:34 +0000

    Set upd db with ElephanSQ\L and deployed to Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn english_wine_cellar.wsgi.application
--- english_wine_cellar/settings.py
+++ english_wine_cellar/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +27,7 @@ SECRET_KEY = 'django-insecure-&ky2$(h&#m&s&px15a2x2-@*xr2(nzga#7at9!g)i*s_48%ibu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['the-english-wine-cellar.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -122,12 +123,17 @@ WSGI_APPLICATION = 'english_wine_cellar.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
