2a49c84ba24d5ed32989c8a126429e33da9042db
Anu Sethi <anu1301@hotmail.co.uk>
2023-03-05 00:49:39 +0000

    Added messages to booking views

--- booking/urls.py
+++ booking/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.view_booking, name='view_booking'),
-    path('book/<item_id>', views.book_now, name='book_now'),
+    path('book/<item_id>', views.add_to_booking, name='add_to_booking'),
+    path('adjust/<item_id>', views.adjust_booking, name='adjust_booking'),
+    path('remove/<item_id>/', views.remove_from_booking, name='remove_from_booking'),
 ]
--- booking/views.py
+++ booking/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render, redirect, reverse
-# get_object_or_404
+from django.shortcuts import (
+    render, redirect, reverse, HttpResponse, get_object_or_404
+)
 from django.contrib import messages
 from datetime import datetime
 
 # from .forms import BookingForm, BookingItemForm
 from .models import BookingItem, Booking
-# from wine_tasting.models import Experiences
+from wine_tasting.models import Experiences
 
 
 def view_booking(request):
@@ -14,33 +15,69 @@ def view_booking(request):
     return render(request, 'booking/booking.html')
 
 
-def book_now(request, item_id):
+def add_to_booking(request, item_id):
     """ Add a number of people and date of the experience to booking """
 
+    experience = get_object_or_404(Experiences, pk=item_id)
     quantity = int(request.POST.get('quantity'))
-    # date = datetime(request.POST.get('date'))
     redirect_url = request.POST.get('redirect_url')
     booking = request.session.get('booking', {})
 
     if item_id in list(booking.keys()):
         booking[item_id] += quantity
-        # messages.success(
-            # request, f'Updated {experiences.name} quantity to {booking[item_id]}')
     else:
         booking[item_id] = quantity
-        # messages.success(request, f'Added {experiences.name} to your booking')
-
-    # if item_id in list(booking.keys()):
-    #     booking[item_id] += date
-    #     messages.success(
-    #         request, f'Updated {experience.name} date to {booking[item_id]}')
-    # else:
-    #     booking[item_id] = date
-    #     messages.success(request, f'Added {experience.name} to your booking')
+        messages.success(request, f'Added {experience.name} to your booking')
 
     request.session['booking'] = booking
     return redirect(redirect_url)
 
+
+def adjust_booking(request, item_id):
+    """
+    Adjust the quantity of the specified experience to the specified amount
+    """
+
+    experience = get_object_or_404(Experiences, pk=item_id)
+    quantity = int(request.POST.get('quantity'))
+    booking = request.session.get('booking', {})
+
+    if quantity > 0:
+        booking[item_id] = quantity
+    else:
+        booking.pop(item_id)
+        messages.success(
+            request, f'Removed {experience.name} from your booking')
+
+    request.session['booking'] = booking
+    return redirect(reverse('view_booking'))
+
+
+def remove_from_booking(request, item_id):
+    """ Removes specified experience from the booking """
+
+    try:
+        experience = get_object_or_404(Experiences, pk=item_id)
+        item = None
+        if 'item_id' in request.POST:
+            item = request.POST['item_id']
+        booking = request.session.get('booking', {})
+
+        if item:
+            del booking[item_id]["item_id"]
+            messages.success(
+                request, f'Removed {experience.name} from your booking')
+        else:
+            booking.pop(item_id)
+            messages.success(
+                request, f'Removed {experience.name} from your booking')
+
+        request.session['booking'] = booking
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
+        return HttpResponse(status=500)
 
 
 # def bookingitem(request):
