99b1a49bd8d21a3637a18c83532f94d12c764945
Anu Sethi <anu1301@hotmail.co.uk>
2023-05-15 19:30:39 +0000

    Added unsubscribe view, url and template

--- newsletter/urls.py
+++ newsletter/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.newsletter_sub, name='newsletter'),
+    path(
+        'newsletter/unsubscribe',
+        views.newsletter_unsubscribe,
+        name='newsletter_unsubscribe'),
 ]
--- newsletter/views.py
+++ newsletter/views.py
@@ -28,4 +28,32 @@ def newsletter_sub(request):
     }
 
     return render(request, 'newsletter/newsletter.html', context)
-   
+
+
+def newsletter_unsubscribe(request):
+    """ A view to handle newsletter unsubscriptions """
+
+    form = NewsLetterForm()
+
+    if request.method == 'POST':
+        form = NewsLetterForm(request.POST)
+        if form.is_valid():
+            instance = form.save(commit=False)
+            if NewsLetterSub.objects.filter(
+                    email=instance.email).exists():
+                NewsLetterSub.objects.filter(
+                    email=instance.email).delete()
+                messages.success(request, 'You have successfully unsubscribed \
+                    from our newsletter, we are sorry to see you go.')
+
+                return redirect(reverse('home'))
+            else:
+                messages.error(request, 'Sorry but we could not find your \
+                    email address. \
+                        Please ensure that it has been entered correctly.')
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'newsletter/newsletter_unsubscribe.html', context)
